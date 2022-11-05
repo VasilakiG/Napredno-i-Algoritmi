346d1344d2d0577265dcad424953f6306206f90a
Vasilaki Gorgioski <vasilakigorgi@gmail.com>
2022-11-05 21:26:32 +0100

    Added MinDistanceException
    Tried to add exceptions, was successful with N being lower than 2, but not fully successful with two dots being the same exception, it catches even when they are not the same.

--- MinDistanceExceptions/src/MinDistance.java
+++ MinDistanceExceptions/src/MinDistance.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+
+
 
 public class MinDistance {
 
-    public static float minimalDistance(float[][] points){
+    public static float minimalDistance(float[][] points) throws Exception {
         //Vasiot kod ovde
         int arrayLength = points.length;
-
+        if(arrayLength < 2){
+            throw new Exception("N is lower than 2!");
+        }
         double minDistance = 9999999999.99;
 
         double x1, y1, x2, y2;
@@ -17,6 +22,9 @@ public class MinDistance {
                 y2 = points [j][1];
                 if( (y2!=y1 || x2!=x1) && Math.sqrt( ((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)) ) < minDistance ){
                     minDistance = Math.sqrt( ((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)) );
+                }
+                else if( y2==y1 && x2==x1 ){
+                    throw new Exception("There are two dots with the same position!");
                 }
             }
         }
@@ -36,6 +44,10 @@ public class MinDistance {
             points[i][1] = input.nextFloat();
         }
 
-        System.out.printf("%.2f\n", minimalDistance(points));
+        try{
+            System.out.printf("%.2f\n", minimalDistance(points));
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
